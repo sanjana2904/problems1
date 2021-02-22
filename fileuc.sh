2bcfe781fab081ff2a084201b81b30c2bcd19a4d
sanjana k <sanjukarthijan27@gmail.com>
2021-02-22 12:32:08 +0530

    added in fileuc.sh

--- fileuc.sh
+++ fileuc.sh
@@ -1,17 +1,14 @@
-#! /bin/bash
+#! /bin/bash 
 
 declare -A dict
 
-#declare -a array
+declare -a array
 
-read -p "enter the values of :" a b c sum
+read -p "enter the values of :" a b c 
 
 sum=$(( $a+$b*$c ))
-
 sum1=$(( $a*$b+$c ))
-
 sum2=$(( $c+$a/$b ))
-
 sum3=$(( $a%$b+$c ))
 
 echo "enter three values as a,b,c"
@@ -26,11 +23,34 @@ echo $sum3
 
 dict=([1]=$sum [2]=$sum1 [3]=$sum2 [4]=$sum3)
 
-echo "dictionary value:" ${dict[@]}
+echo ${dict[@]}
 
-#for (( index=1; index<=4; index++ ))
-#do
-	array[$index]=${dict[$index]}
-#done
-#echo "array values: " ${array[@]}
+
+array[$index]=${dict[$index]}
+
+
+echo "array values : " ${array[@]}
+
+for ((i=1; i<=4; i++ ))
+do
+	for (( j=$((i+1)); j<=4; j++ ))
+	do
+	  if [ $((array[$i])) -lt $((array[$j])) ]
+	   then
+		max=$((array[$i]))
+		array[$i]=$((array[$j]))
+		array[$j]=$max
+	    fi
+	done
+done
+
+echo "array in descending order: " ${array[@]}
+
+
+
+
+
+
+
+
 
